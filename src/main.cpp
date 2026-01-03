4e62d1a25b90793bc717d0f0e8c7dd4e315dd783
georgerrrr <george.ridout05@gmail.com>
2026-01-03 23:41:24 +0000

    add gtk

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,37 @@
 #include <cstdio>
 
+#include <gtkmm/application.h>
+#include <gtkmm/window.h>
+#include <gtkmm/builder.h>
+
+#include "configure.h"
+
 #include "core/audioengine.h"
 
-void oscinit(core::Node&, uint32_t, uint32_t);
-void oscclose();
-
 int main(int argc, char** argv) {
+  auto app = Gtk::Application::create("uk.co.ridoutweb.arlynn");
+
+  auto builder = Gtk::Builder::create_from_file(RESOURCE_PATH "/ui/arlynn.ui");
+  auto main = builder->get_widget<Gtk::Window>("MainWindow");
+
+  auto& audioengine = core::AudioEngine::get();
+
+  app->signal_activate().connect([&] () { 
+      audioengine.initialize(48000, 128);
+
+      app->add_window(*main);
+      main->set_visible();
+      });
+
+  app->signal_shutdown().connect([&] () { audioengine.close(); });
+
+  return app->run(argc, argv);
+  /*
+
+  return app->make_window_and_run<MyWindow>(argc, argv);
   core::AudioEngine::get().initialize(48000, 128);
-
-  getchar();
-  core::AudioEngine::get().addNode(oscinit, oscclose);
-  getchar();
-  core::AudioEngine::get().sendMessage(core::_set_connection{0, 0, 1, 0});
-  getchar();
-  core::AudioEngine::get().sendMessage(core::_set_connection{0, 1, 1, 0});
-  getchar();
-
   core::AudioEngine::get().close();
 
-  return 0;
+  */
 }
 
